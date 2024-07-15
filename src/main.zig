f81bc4c072f1641fc50de876cf5fb05729374ae3
peterc-s <petercs@skiff.com>
2024-07-15 13:22:42 +0100

    improved header handling

--- src/main.zig
+++ src/main.zig
@@ -3,15 +3,40 @@ const network = @import("network");
 
 const BUF_SIZE = 1024;
 
+/// Enum of HTTP status codes.
 const Status = enum {
+    /// 200
     OK,
+    /// 404
     NOT_FOUND,
+    /// 501
+    NOT_IMPLEMENTED,
 };
 
+/// Contains methods for running a HTTP server
+/// on a specified port.
 const HTTPServer = struct {
+    /// The port to run the server on.
     port: u16 = 8080,
-    headers: []u8 = "",
+    /// The headers currently being sent with responses.
+    headers: std.StringHashMap([]u8),
 
+    /// Initialises a server struct with the specified port.
+    pub fn init(port: u16) HTTPServer {
+        var _port = port;
+        if (_port == undefined) {
+            _port = 8080;
+        }
+
+        const headers = std.StringHashMap([]u8).init(std.heap.page_allocator);
+
+        return HTTPServer{
+            .port = _port,
+            .headers = headers,
+        };
+    }
+
+    /// Starts running the server.
     pub fn start(self: HTTPServer) !void {
         var sock = try network.Socket.create(.ipv4, .tcp);
         defer sock.close();
@@ -31,21 +56,19 @@ const HTTPServer = struct {
 
             if (len == 0) continue;
 
-            try handle_request(client, &recv_buf);
+            try self.handle_request(client, &recv_buf);
         }
     }
 
-    fn handle_request(client: network.Socket, data: []u8) !void {
+    /// Handle an incoming request from a client
+    fn handle_request(self: HTTPServer, client: network.Socket, data: []u8) !void {
         std.debug.print("{s}\n", .{std.mem.sliceTo(data[0..], 0)});
 
         const allocator = std.heap.page_allocator;
 
         const response_line = try get_response_line(Status.OK);
 
-        const headers =
-            \\Server: zig-http
-            \\Content-Type: text/html
-        ;
+        const headers = try self.get_headers_str();
 
         const response_body =
             \\<html>
@@ -62,6 +85,29 @@ const HTTPServer = struct {
         std.debug.print("Sending response: \n{s}\n", .{response});
     }
 
+    /// Adds or modifies a header, just a wrapper around self.headers.put().
+    pub fn addHeader(self: *HTTPServer, key: []const u8, value: []const u8) !void {
+        try self.headers.put(key, @constCast(value));
+    }
+
+    /// Compiles a string of the headers in the correct format for a HTTP
+    /// response.
+    pub fn get_headers_str(self: HTTPServer) ![]u8 {
+        var iter = self.headers.iterator();
+        var headers_str = std.ArrayList(u8).init(std.heap.page_allocator);
+        defer headers_str.deinit();
+
+        while (iter.next()) |entry| {
+            try headers_str.appendSlice(entry.key_ptr.*);
+            try headers_str.appendSlice(": ");
+            try headers_str.appendSlice(entry.value_ptr.*);
+            try headers_str.appendSlice("\r\n");
+        }
+
+        return try headers_str.toOwnedSlice();
+    }
+
+    /// Compiles a response line for the given status.
     fn get_response_line(status: Status) ![]u8 {
         const allocator = std.heap.page_allocator;
 
@@ -69,6 +115,7 @@ const HTTPServer = struct {
         const status_str = switch (status) {
             Status.OK => "200 OK",
             Status.NOT_FOUND => "404 Not Found",
+            Status.NOT_IMPLEMENTED => "501 Not Implemented",
         };
 
         const response_line = try std.fmt.allocPrint(allocator, "{s} {s}\r\n", .{ http_prefix, status_str });
@@ -81,6 +128,10 @@ pub fn main() !void {
     try network.init();
     defer network.deinit();
 
-    const serv = HTTPServer{};
+    var serv = HTTPServer.init(8080);
+
+    try serv.addHeader("Server", "zig-http");
+    try serv.addHeader("Content-Type", "text/html");
+
     try serv.start();
 }
